c18417a0e9fdfe07ee40277e98ba1683bb82d0d6
Boet de Willigen <boet.dewilligen@securew2.com>
2021-05-06 17:48:38 +0200

    Add ex_gram dependency

--- mix.exs
+++ mix.exs
@@ -22,8 +22,10 @@ defmodule ImageBot.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_gram, "~> 0.21"},
+      {:tesla, "~> 1.2"},
+      {:hackney, "~> 1.12"},
+      {:jason, ">= 1.0.0"}
     ]
   end
 end
