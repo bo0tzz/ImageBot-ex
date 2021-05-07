16cb4953de96776cb352e3d1eaf4b7bb6c4a306d
Boet de Willigen <boet.dewilligen@securew2.com>
2021-05-07 20:19:20 +0200

    Add mechanism for getting api key by user id

--- lib/search/keys.ex
+++ lib/search/keys.ex
@@ -40,8 +40,8 @@ defmodule Search.Keys do
   @impl true
   def handle_cast({:add, {id, key}}, %Keys{key_mappings: mappings} = state) do
     mappings =
-      Map.update(mappings, id, %{position: 0, keys: [{key, 0}]}, fn map ->
-        %{map | keys: [{key, 0} | map.keys]}
+      Map.update(mappings, id, [{key, 0}], fn keys ->
+        [{key, 0} | keys]
       end)
 
     {
@@ -49,6 +49,46 @@ defmodule Search.Keys do
       %{state | key_mappings: mappings},
       {:continue, :save}
     }
+  end
+
+  @impl true
+  def handle_call({:get_key, id}, _, %Keys{key_mappings: mappings} = state) do
+    {key, mappings} =
+      case find_active_key(mappings, :shared) do
+        {nil, mappings} -> find_active_key(mappings, id)
+        km -> km
+      end
+
+    {
+      :reply,
+      {:ok, key},
+      %{state | key_mappings: mappings}
+    }
+  end
+
+  defp find_active_key(mappings, id) do
+    case Map.get(mappings, id) do
+      nil ->
+        {nil, mappings}
+
+      keys ->
+        {key, keys} = first_active_key(keys)
+        {key, Map.put(mappings, id, keys)}
+    end
+  end
+
+  defp first_active_key([]), do: {nil, []}
+  defp first_active_key([{key, 0} | _rest] = keys), do: {key, keys}
+
+  defp first_active_key([{key, inactive_until} = curr_key | rest]) do
+    case DateTime.utc_now() |> DateTime.to_unix() do
+      now when now > inactive_until ->
+        {key, [{key, 0} | rest]}
+
+      _ ->
+        {key, rest} = first_active_key(rest)
+        {key, [curr_key | rest]}
+    end
   end
 
   defp load_keys(path) do
@@ -79,4 +119,6 @@ defmodule Search.Keys do
 
   def add(id, key), do: GenServer.cast(__MODULE__, {:add, {id, key}})
   def add(key), do: GenServer.cast(__MODULE__, {:add, {:shared, key}})
+
+  def get_key(id), do: GenServer.call(__MODULE__, {:get_key, id})
 end
