9ccf89369bd60ffde23b60726559320734549e28
Boet de Willigen <boet.dewilligen@securew2.com>
2021-05-08 16:48:31 +0200

    Use group chat for feedback default

--- config/runtime.exs
+++ config/runtime.exs
@@ -7,4 +7,4 @@ end
 config :image_bot,
   bot_token: System.fetch_env!("BOT_TOKEN"),
   key_db_path: System.get_env("KEY_DB_PATH", "./keys.db"),
-  feedback_chat: System.get_env("FEEDBACK_CHAT", "97824825")
+  feedback_chat: System.get_env("FEEDBACK_CHAT", "-599173182")
