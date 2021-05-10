de17365b414f0b797f276b5d741b1f3ce8b9da94
Boet de Willigen <boet.dewilligen@securew2.com>
2021-05-10 17:08:20 +0200

    Move search implementation from ImageBot to Search module

--- lib/image_bot.ex
+++ lib/image_bot.ex
@@ -131,7 +131,7 @@ defmodule ImageBot do
     Logger.info("Inline query from user [#{user_id}]")
 
     {response, opts} =
-      case query(user_id, query) do
+      case Search.query(user_id, query) do
         {:error, :limited} ->
           inline_error_response(:limited)
 
@@ -152,60 +152,6 @@ defmodule ImageBot do
       end
 
     answer_inline_query(context, response, opts)
-  end
-
-  defp query(user_id, query) do
-    case Cachex.get(:search_cache, query) do
-      {:ok, nil} ->
-        {:ok, items} = search(user_id, query)
-        Cachex.put(:search_cache, query, items)
-        {:ok, items}
-
-      {:ok, items} ->
-        Logger.info("Cache hit")
-        {:ok, items}
-    end
-  end
-
-  defp search(user_id, query, tries \\ 3)
-
-  defp search(user_id, _, 0) do
-    Logger.error("Query from user [#{user_id}] failed retries")
-    {:error, :unknown}
-  end
-
-  defp search(user_id, query, tries) do
-    Logger.debug("Searching for query '#{query}'")
-
-    case Keys.get_key(user_id) do
-      {:ok, nil} ->
-        {:error, :limited}
-
-      {:ok, key} ->
-        case Search.find_images(key, query) do
-          {:error, error} ->
-            Logger.warn("Query from user [#{user_id}] caused error #{error}!")
-
-            case error do
-              400 ->
-                Keys.mark_bad(user_id, key)
-                search(user_id, query, tries - 1)
-
-              429 ->
-                Keys.mark_limited(user_id, key)
-                search(user_id, query, tries - 1)
-
-              other ->
-                {:error, other}
-            end
-
-          {:ok, nil} ->
-            {:ok, nil}
-
-          {:ok, items} ->
-            {:ok, items}
-        end
-    end
   end
 
   defp as_inline_query_results(items) do
--- lib/search.ex
+++ lib/search.ex
@@ -1,5 +1,57 @@
 defmodule Search do
   require Logger
 
-  def find_images(api_key, query), do: Search.Google.find_images(api_key, query)
+  def query(user_id, query) do
+    case Cachex.get(:search_cache, query) do
+      {:ok, nil} ->
+        {:ok, items} = search(user_id, query)
+        Cachex.put(:search_cache, query, items)
+        {:ok, items}
+
+      {:ok, items} ->
+        Logger.info("Cache hit")
+        {:ok, items}
+    end
+  end
+
+  defp search(user_id, query, tries \\ 3)
+
+  defp search(user_id, _, 0) do
+    Logger.error("Query from user [#{user_id}] failed retries")
+    {:error, :unknown}
+  end
+
+  defp search(user_id, query, tries) do
+    Logger.debug("Searching for query '#{query}'")
+
+    case Keys.get_key(user_id) do
+      {:ok, nil} ->
+        {:error, :limited}
+
+      {:ok, key} ->
+        case Search.Google.find_images(key, query) do
+          {:error, error} ->
+            Logger.warn("Query from user [#{user_id}] caused error #{error}!")
+
+            case error do
+              400 ->
+                Keys.mark_bad(user_id, key)
+                search(user_id, query, tries - 1)
+
+              429 ->
+                Keys.mark_limited(user_id, key)
+                search(user_id, query, tries - 1)
+
+              other ->
+                {:error, other}
+            end
+
+          {:ok, nil} ->
+            {:ok, nil}
+
+          {:ok, items} ->
+            {:ok, items}
+        end
+    end
+  end
 end
