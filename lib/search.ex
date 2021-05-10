d321d26911d542607e1f9fe68ba1ed3eb6a58531
Boet de Willigen <boet.dewilligen@securew2.com>
2021-05-10 17:05:19 +0200

    Move google image search implementation to Search.Google module

--- lib/search.ex
+++ lib/search.ex
@@ -1,27 +1,5 @@
 defmodule Search do
   require Logger
 
-  def find_images(api_key, query) do
-    res =
-      GoogleApi.CustomSearch.V1.Connection.new()
-      |> GoogleApi.CustomSearch.V1.Api.Cse.search_cse_list(params(api_key, query))
-
-    case res do
-      {:error, response} ->
-        Logger.warn("Google API call failed", body: response.body)
-        {:error, response.status}
-
-      {:ok, response} ->
-        {:ok, response.items}
-    end
-  end
-
-  defp params(api_key, query) do
-    [
-      cx: "016322137100648159445:e9nsxf_q_-m",
-      searchType: "image",
-      key: api_key,
-      q: query
-    ]
-  end
+  def find_images(api_key, query), do: Search.Google.find_images(api_key, query)
 end
